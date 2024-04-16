3abb6ee22d637e84e2703e8025e250ef06b27bd8
phạm hùng <cps@May-tinh-xach-tay-cua-pham-hung.local>
2024-04-16 22:33:23 +0700

    new

---
+++ hoctoansieutoc.js
@@ -0,0 +1,142 @@
+function fb(){
+    window.location.href = "https://www.facebook.com/profile.php?id=100041216837070";
+}
+function vaohoc(){
+    window.location.href = "file:///Users/cps/Documents/%C4%91o%CC%82%CC%80%20a%CC%81n%20/vaohoc.html";
+}
+// bài1
+let sai = "sai"
+let dung = "Đúng" 
+ let dapan = function(){
+    let a = document.getElementById("inputa").value
+    if( a == 161){
+        document.getElementById("cautraloia").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloia").innerHTML = sai
+   }
+
+   let b = document.getElementById("inputb").value
+    if( b == 314){
+        document.getElementById("cautraloib").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloib").innerHTML = sai
+   }
+   let c = document.getElementById("inputc").value
+    if( c == 214){
+        document.getElementById("cautraloic").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloic").innerHTML = sai
+   }
+   let d = document.getElementById("inputd").value
+    if( d == 116){
+        document.getElementById("cautraloid").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloid").innerHTML = sai
+   }
+   let e = document.getElementById("inpute").value
+    if( e == 114){
+        document.getElementById("cautraloie").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloie").innerHTML = sai
+   }
+   let f = document.getElementById("inputf").value
+    if( f == 122){
+        document.getElementById("cautraloif").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloif").innerHTML = sai
+   }
+   let g = document.getElementById("inputg").value
+    if( g == 103){
+        document.getElementById("cautraloig").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloig").innerHTML = sai
+   }
+   let h = document.getElementById("inputh").value
+    if( h == 61){
+        document.getElementById("cautraloih").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloih").innerHTML = sai
+   }
+//    bài2
+   let a2 = document.getElementById("inputa2").value
+    if( a2 == 644){
+        document.getElementById("cautraloia2").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloia2").innerHTML = sai
+   }
+   let b2 = document.getElementById("inputb2").value
+    if( b2 == 2826){
+        document.getElementById("cautraloib2").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloib2").innerHTML = sai
+   }
+   let c2 = document.getElementById("inputc2").value
+    if( c2 == 3432){
+        document.getElementById("cautraloic2").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloic2").innerHTML = sai
+   }
+   let d2 = document.getElementById("inputd2").value
+    if( d2 == 464){
+        document.getElementById("cautraloid2").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloid2").innerHTML = sai
+   }
+   let e2 = document.getElementById("inpute2").value
+    if( e2 == 4104){
+        document.getElementById("cautraloie2").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloie2").innerHTML = sai
+   }
+   let f2 = document.getElementById("inputf2").value
+    if( f2 == 5999){
+        document.getElementById("cautraloif2").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloif2").innerHTML = sai
+   }
+   let g2 = document.getElementById("inputg2").value
+    if( g2 == 8352){
+        document.getElementById("cautraloig2").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloig2").innerHTML = sai
+   }
+   let h2 = document.getElementById("inputh2").value
+    if( h2 == 244){
+        document.getElementById("cautraloih2").innerHTML = dung
+    }
+   else
+   {
+    document.getElementById("cautraloih2").innerHTML = sai
+   }
+   }
+ 
